93833b7b3ef71e2d4b81ed0f1b08fd7d2754f496
Elvis S <elvissabanovic3@gmail.com>
2023-07-23 03:00:19 +0200

    feat: finite field addition and subtraction

--- src/ecc/field_element.rs
+++ src/ecc/field_element.rs
@@ -1,13 +1,14 @@
 use std::fmt;
+use std::ops::{Add, Sub};
 
 #[derive(Debug)]
 pub struct FieldElement {
-    num: u128,
-    prime: u128,
+    num: i64,
+    prime: i64,
 }
 
 impl FieldElement {
-    fn new(num: u128, prime: u128) -> Result<FieldElement, String> {
+    fn new(num: i64, prime: i64) -> Result<FieldElement, String> {
         if num >= prime {
             return Err(format!("Num {} not in field range 0 to {}", num, prime - 1));
         }
@@ -24,6 +25,35 @@ impl fmt::Display for FieldElement {
 impl PartialEq for FieldElement {
     fn eq(&self, other: &Self) -> bool {
         self.num == other.num && self.prime == other.prime
+    }
+}
+
+// To satisfy `closed` property one of the tools we can use to make a finite field closed under addition,
+// subtraction, multiplication, and division is modulo arithmetic.
+
+/// a + b = (a + b) % p
+impl Add for FieldElement {
+    type Output = Result<FieldElement, String>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            return Err("Cannot add two numbers in different Fields.".to_string());
+        }
+        let num = (self.num + rhs.num) % self.prime;
+        FieldElement::new(num, self.prime.clone())
+    }
+}
+
+/// a - b = (a - b) % p
+impl Sub for FieldElement {
+    type Output = Result<FieldElement, String>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            return Err("Cannot subtract two numbers in different Fields.".to_string());
+        }
+        let num = (self.num - rhs.num) % self.prime;
+        FieldElement::new(num, self.prime.clone())
     }
 }
 
@@ -44,5 +74,21 @@ mod tests {
 
         assert_eq!(a == a, true);
         assert_eq!(a == b, false);
+    }
+
+    #[test]
+    fn add() {
+        let a = FieldElement::new(7, 19);
+        let b = FieldElement::new(8, 19);
+
+        assert_eq!(a.unwrap() + b.unwrap(), FieldElement::new(15, 19));
+    }
+
+    #[test]
+    fn subtract() {
+        let a = FieldElement::new(11, 19);
+        let b = FieldElement::new(9, 19);
+
+        assert_eq!(a.unwrap() - b.unwrap(), FieldElement::new(2, 19));
     }
 }
