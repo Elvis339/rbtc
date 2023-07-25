923bd60592177e89574f326ac39fbff21bc4f1e8
Elvis S <elvissabanovic3@gmail.com>
2023-07-25 05:46:55 +0200

    feat(ecc): point addition

--- src/ecc/point.rs
+++ src/ecc/point.rs
@@ -2,7 +2,7 @@ use num_bigint::BigInt;
 use std::fmt;
 use std::ops::{Add, Mul};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Point {
     a: BigInt,
     b: BigInt,
@@ -27,24 +27,32 @@ impl Point {
         let x = _x.map(|v| BigInt::from(v));
         let y = _y.map(|v| BigInt::from(v));
 
-        // x^3 + ax + b
-        let curve_form = &x
-            .clone()
-            .unwrap_or(BigInt::from(0))
-            .pow(3)
-            .add(a.clone().mul(&x.clone().unwrap_or(BigInt::from(0))))
-            .add(&b);
-
-        // y^2 = x^3 + ax + b
-        if &y.clone().unwrap_or(BigInt::from(0)).pow(2) != curve_form {
-            return Err(format!(
-                "Point(x={}, y={}) is not on the curve.",
-                &x.unwrap(),
-                &y.unwrap()
-            ));
+        if y.is_none() {
+            return Ok(Point { a, b, x, y: None });
         }
 
-        return Ok(Point { a, b, x, y });
+        return match x {
+            Some(x_val) => {
+                // x^3 + ax + b
+                let curve_form = &x_val.pow(3).add(a.clone().mul(&x_val.clone())).add(&b);
+                if &y.clone().unwrap().pow(2) != curve_form {
+                    return Err(format!(
+                        "Point(a={}, b={}, x={}, y={}) is not on the curve.",
+                        a,
+                        b,
+                        x_val,
+                        y.unwrap(),
+                    ));
+                }
+                return Ok(Point {
+                    a,
+                    b,
+                    x: Some(x_val),
+                    y,
+                });
+            }
+            None => Ok(Point { a, b, x: None, y }),
+        };
     }
 }
 
@@ -52,10 +60,12 @@ impl fmt::Display for Point {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "Point={}={} + {} + {}",
+            "Point({}={} + {} + {})",
             &self.y.clone().unwrap_or(BigInt::from(0)),
             &self.x.clone().unwrap_or(BigInt::from(0)).pow(3),
-            self.a.clone().mul(&self.x.clone().unwrap_or(BigInt::from(1))),
+            self.a
+                .clone()
+                .mul(&self.x.clone().unwrap_or(BigInt::from(1))),
             &self.b
         )
     }
@@ -64,6 +74,73 @@ impl fmt::Display for Point {
 impl PartialEq for Point {
     fn eq(&self, other: &Self) -> bool {
         self.x.eq(&other.x) && self.y.eq(&other.y) && self.a.eq(&other.a) && self.b.eq(&other.b)
+    }
+}
+
+impl Add for Point {
+    type Output = Result<Point, String>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        if self.a != rhs.a || self.b != rhs.b {
+            return Err(format!("{}, {} are not on the same curve.", self, rhs));
+        }
+
+        // If either is none
+        // Some is the point at infinity, or the additive identity.
+        if self.x.is_none() {
+            return Ok(rhs);
+        }
+
+        if rhs.x.is_none() {
+            return Ok(self);
+        }
+        //
+
+        // Additive inverse (same x but different y, causing a vertical line)
+        if self.x.clone().unwrap() == rhs.x.clone().unwrap() {
+            if self.y.is_none() {
+                return Ok(rhs);
+            }
+
+            if rhs.y.is_none() {
+                return Ok(self);
+            }
+        }
+
+        return Err("Invalid point.".to_string());
+    }
+}
+
+// Support &Point + &Point
+impl<'a, 'b> Add<&'b Point> for &'a Point {
+    type Output = Result<Point, String>;
+
+    fn add(self, rhs: &'b Point) -> Self::Output {
+        if self.a != rhs.a || self.b != rhs.b {
+            return Err(format!("{}, {} are not on the same curve.", self, rhs));
+        }
+
+        // If either is none
+        if self.x.is_none() {
+            return Ok(rhs.clone());
+        }
+
+        if rhs.x.is_none() {
+            return Ok(self.clone());
+        }
+
+        // Additive inverse (same x but different y, causing a vertical line)
+        if self.x.clone().unwrap() == rhs.x.clone().unwrap() {
+            if self.y.is_none() {
+                return Ok(rhs.clone());
+            }
+
+            if rhs.y.is_none() {
+                return Ok(self.clone());
+            }
+        }
+
+        return Err("Invalid point.".to_string());
     }
 }
 
@@ -76,7 +153,7 @@ mod tests {
         let p1 = Point::new(-1, -1, Some(5), Some(7));
         assert_eq!(
             p1.unwrap_err(),
-            "Point(x=5, y=7) is not on the curve.".to_string()
+            "Point(a=-1, b=-1, x=5, y=7) is not on the curve.".to_string()
         )
     }
 
@@ -89,7 +166,10 @@ mod tests {
             let not_point = Point::new(5, 7, Some(n.0), Some(n.1));
             assert_eq!(
                 not_point.unwrap_err(),
-                format!("Point(x={}, y={}) is not on the curve.", n.0, n.1)
+                format!(
+                    "Point(a={}, b={}, x={}, y={}) is not on the curve.",
+                    5, 7, n.0, n.1
+                )
             );
         }
 
@@ -100,5 +180,37 @@ mod tests {
             let point = Point::new(5, 7, Some(x.clone()), Some(y.clone()));
             assert_eq!(point.unwrap(), Point::new(5, 7, Some(x), Some(y)).unwrap())
         }
+    }
+
+    #[test]
+    fn error_additive_identity() {
+        let p1 = Point::new(-15, -1, Some(5), Some(7));
+        let p2 = Point::new(0, -76, Some(5), Some(7));
+
+        assert_eq!(
+            p1.unwrap() + p2.unwrap(),
+            Err(
+                "Point(7=125 + -75 + -1), Point(7=125 + 0 + -76) are not on the same curve."
+                    .to_string()
+            )
+        );
+    }
+
+    #[test]
+    fn point_at_infinity() {
+        // When x is None
+        let p1 = Point::new(-15, -1, None, Some(7));
+        let p2 = Point::new(-15, -1, Some(5), Some(7));
+
+        assert_eq!(p1.as_ref().unwrap() + p2.as_ref().unwrap(), p2,);
+    }
+
+    #[test]
+    fn additive_inverse() {
+        // When p1.x == p2.x but y is None
+        let p1 = Point::new(-15, -1, Some(5), Some(7));
+        let p2 = Point::new(-15, -1, Some(5), None);
+
+        assert_eq!(p1.as_ref().unwrap() + p2.as_ref().unwrap(), p1,);
     }
 }
